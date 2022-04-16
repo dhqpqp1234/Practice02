9d4c4631707eb5c622b912c145e7b604143b6ebb
정지웅 <99180134+dhqpqp1234@users.noreply.github.com>
2022-04-17 00:40:33 +0900

    Update Ex09.java

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -10,15 +10,17 @@ public class Ex09 {
 		int num = sc.nextInt();
 		System.out.print("두번째 숫자: ");
 		int num2 = sc.nextInt();
+		int percen = num%num2;
+		int percen1 = num2%num;
 		
-		if (num%num2<1) {
-			System.out.println(num2+"는(은)"+num+" 의 약수입니다.");
-		}else if (num2%num<1){
-			System.out.println(num+"는(은)"+num2+" 의 약수입니다.");
-		}else if(num%num2>1) {
-			System.out.println(num2+"는(은)"+num+" 의 약수가 아닙니다.");
-		}else {
-			System.out.println(num+"는(은)"+num2+" 의 약수가 아닙니다.");
+		if (num>num2 && percen<1) {
+			System.out.println(num2+"는(은)"+num+"의 약수입니다.");
+		}else if(num>num2 && percen>=1) {
+			System.out.println(num2+"는(은)"+num+"의 약수가 아닙니다.");
+		}else if(num2>num && percen1<1) {
+			System.out.println(num+"는(은)"+num2+"의 약수입니다.");
+		}else if(num2>num && percen1>=1){
+			System.out.println(num+"는(은)"+num2+"의 약수가 아닙니다.");
 		}
 		sc.close();
 	}
