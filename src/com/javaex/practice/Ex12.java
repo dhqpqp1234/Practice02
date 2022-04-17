2a61273537968119d0eedc916d9aa7ed12e5f0c7
정지웅 <99180134+dhqpqp1234@users.noreply.github.com>
2022-04-17 16:58:31 +0900

    Update Ex12.java

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -5,34 +5,36 @@ import java.util.Scanner;
 public class Ex12 {
 
 	public static void main(String[] args) {
-		int num1 = 0;
-		int num2 = 0;
-		String sign = "";
+		
 		
 		Scanner sc = new Scanner(System.in);
-			System.out.println("출력되는 내용을 입력하세요");
+				System.out.println("출력되는 내용을 입력하세요");
 			System.out.print("기호: ");
-		sign = sc.nextLine();
-			System.out.println("숫자1: ");
-		num1 = sc.nextInt();
+			String con = sc.nextLine();
+			System.out.print("숫자1: ");
+		double num = sc.nextDouble();
 			System.out.println("숫자2: ");
-		num2 = sc.nextInt();
-		
-		if(sign.equals("+")) {
-			System.out.println("결과는: "+(double)(num1+num2));	
-		}else if(sign.equals("-")) {
-			System.out.println("결과는: "+(double)(num1-num2));	
-		}else if(sign.equals("*")) {
-			System.out.println("결과는: "+(double)(num1*num2));
-		}else if(sign.equals("/")) {
-			System.out.println("결과는: "+(double)num1/num2);
-		}else if(sign.equals("%")) {
-			System.out.println("계싼할 수 없는 기호입니다.");
-		}else if(num1%num2==0){
-			System.out.println("계산할 수 없습니다.");
+		double num1 = sc.nextDouble();
+		double num2 = num/num1;
+		switch (con) {
+			case "+":
+				System.out.println("결과는: "+(num+num1));
+			break;	
+			case "-":
+				System.out.println("결과는: "+(num-num1));
+			break;
+			case "*":
+				System.out.println("결과는: "+(num*num1));
+			break;
+			case "/":
+				System.out.println("결과는: "+num2);
+			
+			break;
+				
+			default :
+				System.out.println("계산할 수 없는 기호입니다.");
+			break;
 		}
-		
-		// 다시해볼 예정입니다.
 		sc.close();
 		
 			}
